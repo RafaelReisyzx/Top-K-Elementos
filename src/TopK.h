454f29b526da3ba36f266963009d74148c4e0d21
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2023-08-20 15:41:01 -0300

    Update TopK.h

--- src/TopK.h
+++ src/TopK.h
@@ -13,11 +13,24 @@ typedef struct Word {
 typedef struct HashTable {
     Word* table[TABLE_SIZE];
 } HashTable;
+typedef struct HeapNode {
+    Word* word_node;
+} HeapNode;
 
+typedef struct Heap {
+    HeapNode* array;
+    int size;
+    int capacity;
+} Heap;
+
+Heap* createHeap(int capacity);
+void insertIntoHeap(Heap* heap, Word* word_node);
+void heapify(Heap* heap, int index);
+Word* extractMinFromHeap(Heap* heap);
+void freeHeap(Heap* heap);
 void initializeHashTable(HashTable* hash_table);
 void loadStopWords(HashTable* hash_table, const char* stop_words_file);
 void processFile(HashTable* hash_table, const char* input_file);
-void printHashTable(const HashTable* hash_table);
 void freeHashTable(HashTable* hash_table);
 
 #endif
