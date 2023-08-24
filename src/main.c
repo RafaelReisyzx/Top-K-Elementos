dc18b7609411a89a3424fda4323cd3f1b4f1a083
RafaelReisyzx <102748011+RafaelReisyzx@users.noreply.github.com>
2023-08-24 11:26:39 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,18 +4,10 @@
 int main() {
     HashTable hash_table;
     initializeHashTable(&hash_table);
-
-    // Carregar as palavras proibidas do arquivo stopwords.txt
     loadStopWords(&hash_table, "dataset/stopwords.txt");
-
-    // Processar o arquivo1.txt e atualizar a tabela hash
     processFile(&hash_table, "dataset/arquivo1.txt");
-
-    // Criar e configurar a heap de tamanho k
-    int k = 10; // O valor de k que você deseja
+    int k = 10; 
     Heap* heap = createHeap(k);
-
-    // Preencher a heap com os primeiros k elementos da tabela hash
     for (int i = 0; i < TABLE_SIZE; i++) {
         Word* current = hash_table.table[i];
         while (current != NULL) {
@@ -29,7 +21,6 @@ int main() {
         }
     }
 
-    // Continuar a análise da tabela hash e atualizar a heap conforme necessário
     for (int i = 0; i < TABLE_SIZE; i++) {
         Word* current = hash_table.table[i];
         while (current != NULL) {
@@ -40,15 +31,12 @@ int main() {
             current = current->next;
         }
     }
-
-    // Imprimir os k elementos com maiores valores (frequências) em ordem crescente (heapsort)
     printf("Top %d palavras mais frequentes:\n", k);
     while (heap->size > 0) {
         Word* max_word = extractMinFromHeap(heap);
         printf("%s: %d\n", max_word->word, max_word->frequency);
     }
 
-    // Liberar memória da heap e da tabela hash
     freeHeap(heap);
     freeHashTable(&hash_table);
 
